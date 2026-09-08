ceea3d68ea20e2c779f6fbda2dba05acf30cdcff
MarcoPrieto <2145717@cegepmontpetit.ca>
2026-09-08 10:33:41 -0400

    new base worker class

---
+++ OMAXRuntimeCollector/RuntimeCollectorWorker.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace OMAXRuntimeCollector;
+
+public class RuntimeCollectorWorker : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // Our existing collector logic will move here.
+    }
+}
